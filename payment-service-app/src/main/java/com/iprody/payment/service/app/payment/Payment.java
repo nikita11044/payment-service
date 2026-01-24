034352e5c40ffab8964c87f841f6b9fbff7e2a38
iskorostinskiinikita <nikita11042000@gmail.com>
2026-01-24 12:29:27 +0300

    Lesson 2. Added payment model & controller

---
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/payment/Payment.java
@@ -0,0 +1,13 @@
+package com.iprody.payment.service.app.payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    private long id;
+    private double value;
+}
