aea0860cab64ed09c93f31bdf891e5046b91c771
iskorostinskiinikita <nikita11042000@gmail.com>
2026-02-13 16:58:41 +0300

    Lesson 8. Added payment filtering, sorting and pagination

---
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/persistence/PaymentSpecifications.java
@@ -0,0 +1,28 @@
+package com.iprody.payment.service.app.persistence;
+
+import com.iprody.payment.service.app.persistence.entity.Payment;
+import com.iprody.payment.service.app.persistence.entity.PaymentStatus;
+import org.apache.commons.collections4.queue.PredicatedQueue;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public final class PaymentSpecifications {
+
+    public static Specification<Payment> hasCurrency(String currency) {
+        return (root, query, cb) -> cb.equal(root.get("currency"), currency);
+    }
+
+    public static Specification<Payment> amountBetween(BigDecimal min, BigDecimal max) {
+        return (root, query, cb) -> cb.between(root.get("amount"), min, max);
+    }
+
+    public static Specification<Payment> createdBetween(Instant after, Instant before) {
+        return (root, query, cb) -> cb.between(root.get("createdAt"), after, before);
+    }
+
+    public static Specification<Payment> hasStatus(PaymentStatus status) {
+        return (root, query, cb) -> cb.equal(root.get("status"), status);
+    }
+}
--- payment-service-app/src/main/java/com/iprody/payment/service/app/service/PaymentService.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/service/PaymentService.java
@@ -1,17 +1,22 @@
 package com.iprody.payment.service.app.service;
 
+import com.iprody.payment.service.app.persistence.PaymentFilter;
 import com.iprody.payment.service.app.persistence.entity.Payment;
+import com.iprody.payment.service.app.persistency.PaymentFilterFactory;
 import com.iprody.payment.service.app.persistency.PaymentRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
+import org.springframework.data.domain.Page;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
+
     private final PaymentRepository paymentRepository;
 
     public Payment getPaymentById(UUID id) {
@@ -20,8 +25,14 @@ public class PaymentService {
 
     }
 
-    public List<Payment> getAllPayments() {
-        return paymentRepository.findAll();
+    public List<Payment> search(PaymentFilter filter) {
+        final Specification<Payment> spec = PaymentFilterFactory.fromFilter(filter);
+        return paymentRepository.findAll(spec);
+    }
+
+    public Page<Payment> searchPaged(PaymentFilter filter, Pageable pageable) {
+        final Specification<Payment> spec = PaymentFilterFactory.fromFilter(filter);
+        return paymentRepository.findAll(spec, pageable);
     }
 }
 
