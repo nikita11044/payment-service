5c171f3e540fe9c8eb1dfecde0b66f9f9fec6e77
iskorostinskiinikita <nikita11042000@gmail.com>
2026-03-06 18:31:33 +0300

    Lesson 14. Added integration tests

--- payment-service-app/src/main/java/com/iprody/payment/service/app/exception/GlobalExceptionHandler.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.iprody.payment.service.app.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,6 +18,14 @@ public class GlobalExceptionHandler {
                 .errorMessage(ex.getMessage())
                 .operation(ex.getOperation())
                 .entityId(ex.getEntityId())
+                .build();
+    }
+
+    @ExceptionHandler({AccessDeniedException.class, AuthorizationDeniedException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorDto handleAccessDenied(Exception ex) {
+        return ErrorDto.builder()
+                .errorMessage("Access Denied")
                 .build();
     }
 
