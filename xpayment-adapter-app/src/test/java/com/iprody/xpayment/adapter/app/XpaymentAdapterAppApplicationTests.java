a4259ab6330c4a5644babd0d72ed14198829a246
iskorostinskiinikita <nikita11042000@gmail.com>
2026-03-30 23:18:05 +0300

    Lesson 19. Added xpayment opeapi config

---
+++ xpayment-adapter-app/src/test/java/com/iprody/xpayment/adapter/app/XpaymentAdapterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iprody.xpayment.adapter.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpaymentAdapterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
